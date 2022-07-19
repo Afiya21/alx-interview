5072f7618377f1996afd5b78dc5cd736241e5fdd
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2022-07-19 01:38:30 -0700

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,41 +1,39 @@
-#!/usr/bin/env python3
-""" The N queens puzzle is the challenge of placing N non-attacking queens on
-    an N×N chessboard. Write a program that solves the N queens problem.
-    Usage: nqueens N
-        If the user called the program with the wrong number of arguments,
-        print Usage: nqueens N, followed by a new line,
-        and exit with the status 1
-    where N must be an integer greater or equal to 4
-        If N is not an integer, print N must be a number,
-        followed by a new line, and exit with the status 1
-        If N is smaller than 4, print N must be at least 4,
-        followed by a new line, and exit with the status 1
-    The program should print every possible solution to the problem
-        One solution per line
-        You don’t have to print the solutions in a specific order
-    You are only allowed to import the sys module """
+#!/usr/bin/python3
+""" program that solves the N queens problem """
 import sys
 
 
-def nqueens(n: int):
-    """
-    backtracking
-    """
-    matrix = [[0 for x in range(n)] for y in range(n)]
-    print(str(matrix))
+def isSafe(Board, line, i):
+    """ checks if we can insert queen at column i in that line in Board"""
+    for x in range(line):
+        if (Board[x] == i or
+                Board[x] + line - x == i or
+                Board[x] + x - line == i):
+            return False
+    return True
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 2 or len(sys.argv) < 2:
-        print("Usage: nqueens N")
-        exit(1)
+def Fill_line(Board, line):
+    """ fills each line of the board with the correct index """
+    for i in range(len(Board)):
+        if isSafe(Board, line, i):
+            Board[line] = i
+            if line < len(Board) - 1:
+                Fill_line(Board, line + 1)
+            else:
+                print([[i, Board[i]] for i in range(len(Board))])
 
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    nqueens(int(sys.argv[1]))
+Board = [-1 for i in range(n)]
+Fill_line(Board, 0)
